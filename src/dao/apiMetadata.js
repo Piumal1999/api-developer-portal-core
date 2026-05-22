5262f5770a893f94ca238b11b1ffb80d5ad7e669
Piumal Rathnayake <piumal1999@gmail.com>
2026-05-22 08:51:27 +0530

    Prevent refId from being cleared during subscription plan update

--- src/dao/apiMetadata.js
+++ src/dao/apiMetadata.js
@@ -593,6 +593,9 @@ const updateSubscriptionPolicy = async (orgID, policyID, policy, t) => {
     // Don’t update primary keys
     delete row.ORG_ID;
     delete row.POLICY_ID;
+    if (!Object.prototype.hasOwnProperty.call(policy, 'refId')) {
+      delete row.REF_ID;
+    }
 
     const [_, updatedRows] = await SubscriptionPolicy.update(row, {
       where: { POLICY_ID: policyID, ORG_ID: orgID },
